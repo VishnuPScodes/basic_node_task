f4246fe128061c4d6057ea53c7665fa18a024fac
VishnuPS <psvishnu373@gmail.com>
2024-04-17 21:36:53 +0530

    feat initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectToDb } from './src/configs/db.js';
+import authRouter from './src/routes/auth.routes.js';
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+app.use(cors());
+
+app.use('/auth', authRouter);
+
+app.listen(PORT, async () => {
+  try {
+    await connectToDb(MONGO_URL);
+    console.log('listening to the port ', PORT);
+  } catch (error) {
+    console.log('failed to connect to the database', error);
+  }
+});
